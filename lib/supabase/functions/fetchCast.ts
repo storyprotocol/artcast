75dd0b2d441474005e2a9352b29e1cba26fb201b
jacob-tucker <jacobtucker818@gmail.com>
2024-02-13 17:03:58 -0500

    back to original

--- lib/supabase/functions/fetchCast.ts
+++ lib/supabase/functions/fetchCast.ts
@@ -15,14 +15,14 @@ export async function fetchCast(castId: number): Promise<Cast | null> {
     }
 
 
-    // const { data: versionHistory } = await supabaseClient.rpc('fetchbranch', { leaf_id: castId })
+    const { data: versionHistory } = await supabaseClient.rpc('fetchbranch', { leaf_id: castId })
 
     let ans = {
         ...data[0],
         num_derivatives: td[0].number_of_direct_children,
         num_total_derivatives: td[0].number_of_children,
-        // latest_prompts: await latestPrompts(castId),
-        // version_history: versionHistory
+        latest_prompts: await latestPrompts(castId),
+        version_history: versionHistory
     }
     ans.locked = (ans.branch_num == 1 && ans.locked == true) || (ans.branch_num >= 2 && ans.layer_1_cast.locked == true);
 
