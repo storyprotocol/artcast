50dd4c68454fea64977dbc46ea57ce629997bb07
jacob-tucker <jacobtucker818@gmail.com>
2024-02-13 15:34:52 -0500

    fix: show on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
   }
 
   return (
-    <form onSubmit={submit} className="hidden space-y-6 p-10 pb-16 md:block">
+    <form onSubmit={submit} className="space-y-6 p-10 pb-16">
       <div className="space-y-0.5">
         <h2 className="text-2xl font-bold tracking-tight">Create Artcast</h2>
         <p className="text-muted-foreground">Create your own Artcast and share it on Farcaster below.</p>
