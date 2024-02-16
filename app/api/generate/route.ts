e0e824bc0e471a0bdad4567224c5d05a0c12bf55
jacob-tucker <jacobtucker818@gmail.com>
2024-02-16 12:47:21 -0500

    configure max duration

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -4,14 +4,6 @@ import { NextRequest, NextResponse } from 'next/server';
 export const maxDuration = 30; // This function can run for a maximum of 5 seconds
 export const dynamic = 'force-dynamic';
 
-export async function GET(request: NextRequest) {
-    // Send a success response
-    return new NextResponse(JSON.stringify({ message: 'Image generation successful' }), {
-        status: 200
-    })
-}
-
-
 export async function POST(request: NextRequest) {
     const { castName, prompts, createdArtcastId, farcasterName } = await request.json();
 
