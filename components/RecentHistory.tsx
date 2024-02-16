1a048bc03eef0030508a2301c73c43ac42713bc3
Andrea Muttoni <maverickmuttoni+github@gmail.com>
2024-02-16 14:07:53 +0100

    - changed dashboard title to the artcast ID - improved story protocol badging - clarified copy throughout playing on the the concepts or remixing and storylines. - added a share button in the main page of an artcast - general fixes a bit all over the place

--- components/RecentHistory.tsx
+++ components/RecentHistory.tsx
@@ -19,25 +19,23 @@ async function Version({ cast, index, latest }: { cast: Cast, index: number, lat
             <span className="absolute flex items-center justify-center w-6 h-6 bg-yellow-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-yellow-900 text-sm">
                 {cast.branch_num}
             </span>
-            <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-                {cast.branch_num == 0 ? <span className="">Original Artcast</span> : <span className="italic">"{cast.prompt_input}"</span>}
+            <a href={`/cast/${cast.id}`}><h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                {cast.branch_num == 0 ? <span className="">{cast.prompt_input ? cast.prompt_input : "Original Artcast"}</span> : <span className="italic">"{cast.prompt_input}"</span>}
                 {latest
-                    ? <span className="bg-yellow-100 text-yellow-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300 ms-3">Latest</span>
+                    ? <span className="bg-yellow-100 text-yellow-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300 ms-3">Current</span>
                     : null
                 }
-            </h3>
-            <time className="block text-sm mb-1 font-normal leading-none text-gray-400 dark:text-gray-500">Created on {convertSupabaseDateToHumanReadable(cast.created_at)}</time>
-            <p className="block text-sm font-normal leading-none text-gray-400 dark:text-gray-500">by <AuthorLink farcasterId={cast.farcaster_id} /></p>
+            </h3></a>
+            <p className="block text-sm font-normal leading-none text-gray-400 dark:text-gray-500">by <AuthorLink farcasterId={cast.farcaster_id} /> <time className="text-xs font-normal leading-none text-gray-400 dark:text-gray-500">on {convertSupabaseDateToHumanReadable(cast.created_at)}</time> </p>
             <RegisteredOnStory storyExplorerUrl={cast.story_explorer_url} />
             <div className="flex items-center gap-5">
                 <img className="w-[25%] max-w-[200px] h-auto rounded-md" src={url} alt={`cast ${cast.name}`} />
-                {latest ?
+                {
                     <ShareButton castId={cast.id}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-3.5 h-3.5 me-2.5">
                             <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                        </svg>Share
+                        </svg>Continue from here
                     </ShareButton>
-                    : null
                 }
             </div>
         </li>
@@ -46,10 +44,10 @@ async function Version({ cast, index, latest }: { cast: Cast, index: number, lat
 
 export function RecentHistory({ versions }: { versions: Cast[] }) {
     return (
-        <div className="rounded-xl border bg-card text-card-foreground shadow p-10">
+        <div className="rounded-xl border bg-card text-card-foreground shadow p-6">
             <div className="flex flex-col space-y-1.5 mb-5">
-                <h3 className="font-semibold leading-none tracking-tight">Timeline (this branch)</h3>
-                <p className="text-sm text-muted-foreground">Below are all the versions of this Artcast up to this point.</p>
+                <h3 className="font-semibold leading-none tracking-tight">This Artcast's Storyline</h3>
+                <p className="text-sm text-muted-foreground">Below are all the direct remixes of this Artcast up to this point.</p>
             </div>
             <ol className="relative border-s border-gray-200 dark:border-gray-700">
                 {versions.reverse().map((version, index) => <Version key={index} index={index} cast={version} latest={index == versions.length - 1}></Version>)}
--- components/RegisteredOnStory.tsx
+++ components/RegisteredOnStory.tsx
@@ -4,13 +4,13 @@ import { Button } from "./ui/button";
 export async function RegisteredOnStory({ storyExplorerUrl }: { storyExplorerUrl: string | null }) {
     if (!storyExplorerUrl) {
         return (
-            <p className="text-sm text-muted-foreground flex gap-1 items-center">Registered on <img src="/story-protocol.png" alt="story protocol logo" className="h-[10px]" /></p>
+            <p className="text-[8px] text-muted-foreground inline-flex gap-1 items-center border py-1 px-2 rounded my-2">Registered on <br/><img src="/story-protocol.png" alt="story protocol logo" className="h-[8px]" /></p>
         )
     }
     return (
         <Button asChild variant={'link'} className="p-0 m-0">
             <Link style={{ height: 0 }} href={storyExplorerUrl} target="_blank">
-                <p className="block text-sm mb-4 font-normal leading-none text-gray-400 dark:text-gray-500 flex gap-1 items-center">Registered on <img src="/story-protocol.png" alt="story protocol logo" className="h-[10px]" /></p>
+                <p className="block text-xs mb-4 font-normal leading-none text-gray-400 dark:text-gray-500 flex gap-1 items-center">Registered on <img src="/story-protocol.png" alt="story protocol logo" className="h-[8px]" /></p>
             </Link>
         </Button>
     )
